dd23ac8b03ca308bf1264edf2c23fed02f03b44a
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2016-01-09 09:22:00 +0100

    fix alphanumeric?

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject org.clojars.magomimmo/valip "0.5.0-SNAPSHOT"
-  :description "Functional validation library for Clojure and ClojureScript, forked from https://github.com/cemerick/valip"
+(defproject org.clojars.magomimmo/valip "0.4.0-SNAPSHOT"
+  :description "Functional validation library for Clojure and ClojureScript. 
+                Forked from https://github.com/cemerick/valip"
   :url "http://github.com/magomimmo/valip"
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :clean-targets ^{:protect false} ["resources" "dev-resources" :target-path])
