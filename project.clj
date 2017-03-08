20d2e11edb763970af2c0ba7944f79c7fd020617
Manuela <manuela@Ubuntu.station>
2017-03-08 17:06:43 +0100

    update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
+
 (defproject org.clojars.magomimmo/valip "0.4.0-SNAPSHOT"
   :description "Functional validation library for Clojure and ClojureScript, forked from https://github.com/cemerick/valip"
   :url "http://github.com/magomimmo/valip"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :clean-targets ^{:protect false} ["resources" "dev-resources" :target-path])
